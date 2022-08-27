cd40caec32bd9b25c3bc1473ec5c1b9ce061fac2
SKAUL05 <kaul.sarath@gmail.com>
2022-08-27 15:15:39 +0530

    LRU Cache Added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LRU
+
+## Implement Least Recently Used (LRU) cache.
+
+### You need to implement the following for the LRUCache class:
+
+
+- `LRUCache(int capacity)` initializes the cache to store data of size: capacity.
+- `int get(int key)` returns the value of the key if it exists, otherwise returns -1.
+- `void add(int key, int value)` updates the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity
+from this operation, evict the least recently used key.
+
+#### Note: Try to achieve each operation in O(1) time complexity.
---
+++ lru.py
@@ -0,0 +1,51 @@
+import collections
+
+
+class LRU:
+    """ Least Recently Used Cache """
+
+    def __init__(self, capacity: int):
+        """ Initialize cache to store data
+
+        :param capacity: cache size
+        """
+        self.remaining = capacity
+        self.lru_cache = collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        """ Return the value of key in cache
+        :param key: Search key
+        :return: Value
+        """
+        if key in self.lru_cache:
+            value = self.lru_cache.pop(key)
+            self.lru_cache[key] = value
+            return value
+        return -1
+
+    def add(self, key: int, value: int) -> None:
+        """ Add value in cache if not exists otherwise update the current value
+        :param key: Cache Key
+        :param value: Cache Value
+        :return:
+        """
+        if key in self.lru_cache:
+            self.lru_cache.pop(key)
+        else:
+            if self.remaining == 0:
+                self.lru_cache.popitem(last=False)
+            else:
+                self.remaining -= 1
+        self.lru_cache[key] = value
+
+
+if __name__ == "__main__":
+    cache = LRU(3)
+    print(cache.get(2))  # Expected: -1
+    cache.add(2, 1)
+    print(cache.get(2))  # Expected: 1
+    cache.add(3, 1)
+    cache.add(4, 2)
+    cache.add(5, 3)
+    cache.add(6, 4)
+    print(cache.get(2))  # Expected: -1
